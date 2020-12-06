8417846902e2021aac6060fc629876c1c040b3b4
mj <1833789622@qq.com>
2020-12-06 22:51:52 +0800

    测试git新增文件

---
+++ src/main/java/com/offcn/controller/TestGitController.java
@@ -0,0 +1,15 @@
+package com.offcn.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mj
+ * @version 1.0
+ * @date 2020/12/6 22:50
+ */
+@RestController
+public class TestGitController {
+    public void test(){
+        System.out.println("测试新增文件！");
+    }
+}
